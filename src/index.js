28efd154a8fbf3c3d5efe3c042b010383548377f
Josh Kim <joshua940308@gmail.com>
2019-08-14 14:26:53 -0400

    added reactime

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Reactime from "reactime";
 import App from "./component/App";
 import "./index.css";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+Reactime(document.getElementById("root"));
