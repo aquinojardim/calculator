c127032fc3514a2c465ca43dbefbc19e14bf628f
aquinojardim <64516365+aquinojardim@users.noreply.github.com>
2020-06-29 21:58:00 -0700

    test

--- src/component/App.js
+++ src/component/App.js
@@ -1,26 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
 import calculate from "../logic/calculate";
 import "./App.css";
 
-export default class App extends React.Component {
-  state = {
-    total: null,
-    next: null,
-    operation: null,
+const App = props => {  
+  const [obj, setObj] = useState({ total: null, next: null, operation: null });
+
+  const handleClick = buttonName => {
+    setObj(calculate(obj, buttonName));
   };
 
-  handleClick = buttonName => {
-    this.setState(calculate(this.state, buttonName));
-  };
-
-  render() {
-    return (
-      <div className="component-app">
-        <Display value={this.state.next || this.state.total || "0"} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </div>
-    );
-  }
+  return (
+    <div className="component-app">
+      <Display value={obj.next || obj.total || "0"} />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
 }
+
+export default App
